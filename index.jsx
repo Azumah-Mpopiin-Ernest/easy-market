d219e99624fbf78ae6b99c7dd77960879074613b
Tesla Junior <teslajunior0552@gmail.com>
2025-11-15 19:41:32 +0000

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -6,10 +6,3 @@ root.render(
     <App />
 )
 
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js")
-      .catch((err) => console.log("Service Worker registration failed: ", err));
-  });
-}
