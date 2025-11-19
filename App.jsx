de52c80cb3a18d7f087017e219cae0fc12edc4a7
Tesla Junior <teslajunior0552@gmail.com>
2025-11-19 00:31:07 +0000

    Change Analytics import from next to react

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import MainComponent from "./mainComponent";
 export default function App() {
   return (
